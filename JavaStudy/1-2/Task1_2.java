2f04068296b387c92881f813cf2910abeb08d5ef
OyamaSayaka <84369797+OyamaSayaka@users.noreply.github.com>
2021-06-05 08:40:07 +0900

    Task1-2

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,50 @@
+/**
+ * Task1-2 : 課題内容
+ *
+ * 本課題では、配列の使い方を学んでいきましょう。
+ * 問①から問⑥まであります。
+ * 指定された値と変数名を守って記述してください。
+ *
+ * 問⑤,⑥については、コメントを入れてください。
+ *
+ */
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+        // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
+        String[] country = {"JAPAN","AMERICA","KOREAN"};
+      
+        
+
+        // ② ①で作成した配列の要素数を出力してください。
+        System.out.println(country.length);
+
+        // ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
+        //   1番目（先頭）の要素に 「りんご」 を代入
+         country[0]= "りんご";
+
+        //  2番目の要素に 「もも」 を代入
+         country[1]= "もも";
+
+
+        // 3番目の要素に 「ぶどう」 を代入
+         country[2]= "ぶどう";
+         
+
+        // ④ ③で作成した配列の2番目の要素を出力しなさい。
+        System.out.println(country[1]);
+
+
+
+        /* ⑤下記の処理について、何をしているのかコメントを記入してください。
+         *  [int[]型の変数intArrayの宣言と初期化 ]
+         */
+        int[] intArray = { 10, 20, 30, 40, 50 };
+
+        // ⑥下記の処理について、何をしているのかコメントを記入してください。
+        // [intArray[1] (20)　とintArray[4](50)の合計を出力する指示をしている。 ]
+        System.out.println(intArray[1] + intArray[4]);
+
+    }
+}
