017280803100a0848a5d2e433271f84b8a5682b3
OyamaSayaka <84369797+OyamaSayaka@users.noreply.github.com>
2021-06-15 11:59:46 +0900

    Task1-4 再提出

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -13,8 +13,8 @@ public class Task1_4 {
 
     // 定数（アカウント情報）
     // ① 登録されている名前（USER_NAME）とパスワード（USER_PASSWORD）を定数で定義してください。
-    private static final String USER_NAME = "oyama";
-    private static final String USER_PASSWORD = "oyama123";
+    private static final String USER_NAME = "alice";
+    private static final String USER_PASSWORD = "alice123";
 
     // 定数（メッセージ）
     private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
@@ -41,32 +41,34 @@ public class Task1_4 {
         //      定数を使用して「 ログイン成功です 」と出力してください。
 
         if (name.equals(USER_NAME) && pass.equals(USER_PASSWORD )) {
-          System.out.println("ログイン成功です。");
-        }
+          System.out.println(CONST_MSG_SUCCESS);
+        
 
 
         // ③ 「USER_RNAME」の値のみ等しい場合。
        //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。 
-       if (name.equals(USER_NAME)) {
+        } else if (name.equals(USER_NAME)) {
          System.out.println(CONST_MSG_ERROR_PASS);
-       }
+       
     
     
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
-         if (pass.equals(USER_PASSWORD)) {
+          } else if (pass.equals(USER_PASSWORD)) {
            System.out.println(CONST_MSG_ERROR_NAME);
 
-         }
-
-    
+         
 
         // ⑤ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。 
        //定数を使用して「 入力情報に誤りがあります。 」と出力してください。
-         if (!(name.equals(USER_NAME)) && !(pass.equals(USER_PASSWORD))) {
+      
+          } else {
            System.out.println(CONST_MSG_ERROR_INPUT);
         }
+        
+
     }
 
-}
 
+
+    }
